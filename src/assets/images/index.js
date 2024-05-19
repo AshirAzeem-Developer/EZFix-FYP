e1e901e5c43b7f640e394724ec9dd248277f12c6
Huzaifa <huzaifajt2525@gmail.com>
2024-05-19 23:35:59 +0500

    seeker detail done

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -48,5 +48,6 @@ export default {
   Plumber: require('./Plumber.png'),
   ViewAll: require('./ViewAll.png'),
   Waiter: require('./Waiter.png'),
-  SeekerImg:require('./Seekerimg.png')
+  SeekerImg:require('./Seekerimg.png'),
+  DemoSeeker:require('./DemoSeeker.png')
 };
--- src/navigators/stack.navigator.js
+++ src/navigators/stack.navigator.js
@@ -17,13 +17,14 @@ import OTP from '../screens/Auth/OTP';
 import {BottomNavigator} from './bottom.navigator';
 import Categories from '../screens/Seekers/Categories';
 import CategorySelect from '../screens/Seekers/CategorySelect';
+import SeekerDetails from '../screens/Seekers/SeekerDetails';
 
 const Stack = createNativeStackNavigator();
 
 export const RootNavigator = ({}) => {
   return (
     <Stack.Navigator
-      initialRouteName="CategorySelect"
+      initialRouteName="SeekerDetails"
       screenOptions={{
         headerShown: false,
       }}>
@@ -37,6 +38,8 @@ export const RootNavigator = ({}) => {
       <Stack.Screen name="Congrats" component={Congrats} />
       <Stack.Screen name="Categories" component={Categories} />
       <Stack.Screen name="CategorySelect" component={CategorySelect} />
+      <Stack.Screen name="SeekerDetails" component={SeekerDetails} />
+
 
 
 
